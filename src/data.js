1d315fd32da6421436c9f607560568cc871388ae
johnlombardi389 <lombardij38@gmail.com>
2022-11-17 18:51:19 -0500

    Created data file containing all song information and audio

---
+++ src/data.js
@@ -0,0 +1,98 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop() {
+  return [
+    {
+      id: uuidv4(),
+      name: "Romance",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/03/75adfe0661d06a9ea66d9c2e99b31e92ae450ebe-1024x1024.jpg",
+      artist: "RenBoz",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=16064",
+      color: ["#F9B339", "#F54933"],
+      active: true,
+    },
+    {
+      id: uuidv4(),
+      name: "Where The Sun Goes",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/09/6a9ef8ac00e168d3308fdf9e336874c03fab829d-1024x1024.jpg",
+      artist: "Delayde, anybodyy",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=23188",
+      color: ["#B1F8A8", "#8A62D5"],
+      active: false,
+    },
+    {
+      id: uuidv4(),
+      name: "Lunar Cycles",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/c209a7df7b9bc133dfff73ce86ebc3c57c2b73dd-1024x1024.jpg",
+      artist: "Sleepy Fish",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10031",
+      color: ["#41A5BD", "#302C4D"],
+      active: false,
+    },
+    {
+      id: uuidv4(),
+      name: "Mario Kart",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/03/74d62bc9370a68e440c1b98eaf650344f0a7faea-1024x1024.jpg",
+      artist: "SwuM",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=15228",
+      color: ["#283432", "#964A2A"],
+      active: false,
+    },
+    {
+      id: uuidv4(),
+      name: "Cabaña",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/07/4163ebb931e06d4ee8eb184295c0246d4a5055f7-1024x1024.jpg",
+      artist: "Juan Rios",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=21647",
+      color: ["#FF8E73", "#3F2240"],
+      active: false,
+    },
+    {
+      id: uuidv4(),
+      name: "Cascade",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/02/70a56749b8b89815fa75446030c6ba57d2c34de7-1024x1024.jpg",
+      artist: "Knowmadic",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=13092",
+      color: ["#015754", "#FB8B65"],
+      active: false,
+    },
+    {
+      id: uuidv4(),
+      name: "Dreamlover",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/02/d12927eedcc2f5afba2ab049a4a1ea46c2266ae3-1024x1024.jpg",
+      artist: "C Y G N",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=14995",
+      color: ["#88BBEA", "#E58FF0"],
+      active: false,
+    },
+    {
+      id: uuidv4(),
+      name: "Viatecture",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/06/23b2ff959731b56ea8545828b462311e8b1a2bcc-1024x1024.jpg",
+      artist: "Leavv, Makzo",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=20119",
+      color: ["#AEB884", "#894E50"],
+      active: false,
+    },
+    {
+      id: uuidv4(),
+      name: "Under the City Stars",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+      color: ["#174E4B", "#D67252"],
+      active: false,
+    },
+  ];
+}
+
+export default chillHop;
