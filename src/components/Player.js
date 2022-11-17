22e01c647d2160b166aa06cddf4ad7b4d32acdb7
johnlombardi389 <lombardij38@gmail.com>
2022-11-17 14:18:06 -0500

    Imported font-awesome icons for play, backward, and forward and added to Player component

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,10 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Player() {
   return (
@@ -8,7 +14,19 @@ function Player() {
         <input type="range" />
         <p>End Time</p>
       </div>
-      <div className="play-control"></div>
+      <div className="play-control">
+        <FontAwesomeIcon
+          className="skip-backward"
+          size="2x"
+          icon={faAngleLeft}
+        />
+        <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+        <FontAwesomeIcon
+          className="skip-forward"
+          size="2x"
+          icon={faAngleRight}
+        />
+      </div>
     </div>
   );
 }
