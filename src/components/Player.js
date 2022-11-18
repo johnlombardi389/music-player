b6d23b3524d0516d59097a5773293e3036e19bbd
johnlombardi389 <lombardij38@gmail.com>
2022-11-17 19:25:40 -0500

    Connected data audio to player component and added useRef for audio element

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPlay,
@@ -6,7 +6,11 @@ import {
   faAngleRight,
 } from "@fortawesome/free-solid-svg-icons";
 
-function Player() {
+function Player({ currentSong }) {
+  const audioRef = useRef(null);
+
+  const playSongHandler = () => {};
+
   return (
     <div className="player">
       <div className="time-control">
@@ -20,13 +24,19 @@ function Player() {
           size="2x"
           icon={faAngleLeft}
         />
-        <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+        <FontAwesomeIcon
+          onClick={playSongHandler}
+          className="play"
+          size="2x"
+          icon={faPlay}
+        />
         <FontAwesomeIcon
           className="skip-forward"
           size="2x"
           icon={faAngleRight}
         />
       </div>
+      <audio ref={audioRef} src={currentSong.audio}></audio>
     </div>
   );
 }
