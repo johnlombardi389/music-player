965d334d6b948c851172e5ad3608ccec4772fdef
johnlombardi389 <lombardij38@gmail.com>
2022-11-18 14:48:06 -0500

    Added pause icon and used ternary operator to check if song is playing and display play or pause button

--- src/components/Player.js
+++ src/components/Player.js
@@ -4,6 +4,7 @@ import {
   faPlay,
   faAngleLeft,
   faAngleRight,
+  faPause,
 } from "@fortawesome/free-solid-svg-icons";
 
 function Player({ currentSong, isPlaying, setIsPlaying }) {
@@ -70,7 +71,7 @@ function Player({ currentSong, isPlaying, setIsPlaying }) {
           onClick={playSongHandler}
           className="play"
           size="2x"
-          icon={faPlay}
+          icon={isPlaying ? faPause : faPlay}
         />
         <FontAwesomeIcon
           className="skip-forward"
