40c1f03b47822797c8a3ca77b7e9c556b087b8f4
johnlombardi389 <lombardij38@gmail.com>
2022-11-20 14:33:04 -0500

    Added new songs to data.js

--- src/data.js
+++ src/data.js
@@ -4,13 +4,23 @@ function chillHop() {
   return [
     {
       id: uuidv4(),
+      name: "Lunar Cycles",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/c209a7df7b9bc133dfff73ce86ebc3c57c2b73dd-1024x1024.jpg",
+      artist: "Sleepy Fish",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10031",
+      color: ["#41A5BD", "#302C4D"],
+      active: true,
+    },
+    {
+      id: uuidv4(),
       name: "Vitamin D",
       cover:
         "https://chillhop.com/wp-content/uploads/2021/07/3b73a510169f14c4af83ac4016e809917412702b-1024x1024.jpg",
       artist: "fantompower",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=22790",
       color: ["#DBE7E3", "#D2AED4"],
-      active: true,
+      active: false,
     },
     {
       id: uuidv4(),
@@ -34,12 +44,12 @@ function chillHop() {
     },
     {
       id: uuidv4(),
-      name: "Lunar Cycles",
+      name: "Viatecture",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/c209a7df7b9bc133dfff73ce86ebc3c57c2b73dd-1024x1024.jpg",
-      artist: "Sleepy Fish",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10031",
-      color: ["#41A5BD", "#302C4D"],
+        "https://chillhop.com/wp-content/uploads/2021/06/23b2ff959731b56ea8545828b462311e8b1a2bcc-1024x1024.jpg",
+      artist: "Leavv, Makzo",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=20119",
+      color: ["#AEB884", "#894E50"],
       active: false,
     },
     {
@@ -80,16 +90,6 @@ function chillHop() {
       artist: "C Y G N",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=14995",
       color: ["#88BBEA", "#E58FF0"],
-      active: false,
-    },
-    {
-      id: uuidv4(),
-      name: "Viatecture",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2021/06/23b2ff959731b56ea8545828b462311e8b1a2bcc-1024x1024.jpg",
-      artist: "Leavv, Makzo",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=20119",
-      color: ["#AEB884", "#894E50"],
       active: false,
     },
     {
@@ -140,6 +140,76 @@ function chillHop() {
       artist: "Poldoore",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=36942",
       color: ["#C79500", "#2B426C"],
+      active: false,
+    },
+    {
+      id: uuidv4(),
+      name: "Pampaya",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/05/d8344de22563af8eaec8f544c14711592eabef26-1024x1024.jpg",
+      artist: "Plusma, Guillaume Muschalle",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=28899",
+      color: ["#69A0A3", "#FABC81"],
+      active: false,
+    },
+    {
+      id: uuidv4(),
+      name: "Higher",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/05/2473c60e471fe9b40e246bf7711c87d3d6ea69a7-1024x1024.jpg",
+      artist: "Misha, Jussi Halme",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=15088",
+      color: ["#E87675", "#2E365B"],
+      active: false,
+    },
+    {
+      id: uuidv4(),
+      name: "Passing Notes",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/07/5821e04fd52fa668a0b9890f213cbb12e454cc6b-1024x1024.jpg",
+      artist: "Middle School, Aso",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=22938",
+      color: ["#3D4F5B", "#CE6C49"],
+      active: false,
+    },
+    {
+      id: uuidv4(),
+      name: "New Moon",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/05/732128e1da8fd5f6292ffc1926a2ea840a54f4d0-1024x1024.jpg",
+      artist: "Toonorth",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=17931",
+      color: ["#FFE76D", "#9A9AFA"],
+      active: false,
+    },
+    {
+      id: uuidv4(),
+      name: "Lemon",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/53eef8b1ac9bf40038b2b5dc7ca2d13fc19cf95e-1024x1024.jpg",
+      artist: "Tom Doolie",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=7977",
+      color: ["#7D2D36", "#655292"],
+      active: false,
+    },
+    {
+      id: uuidv4(),
+      name: "Vinho Verde",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ae91385fc2d92e40dd4bf337867dee16fb5648d2-1024x1024.jpg",
+      artist: "Clap Cotton",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9447",
+      color: ["#394F2B", "#E8F9B5"],
+      active: false,
+    },
+    {
+      id: uuidv4(),
+      name: "Waybackwhen",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/11/4c9682ee612a3b8ef51de286c49b5489408e9257-1024x1024.jpg",
+      artist: "Philanthrope, Parkbench Epiphany",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=27502",
+      color: ["#5D7985", "#EF9796"],
       active: false,
     },
   ];
