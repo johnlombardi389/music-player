f3b85d438aa6bfb2900219747ca6f2fe5712d4ed
johnlombardi389 <lombardij38@gmail.com>
2024-07-21 15:17:42 -0400

    Updated all cover art links

--- src/data.js
+++ src/data.js
@@ -32,8 +32,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Where The Sun Goes",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2021/09/6a9ef8ac00e168d3308fdf9e336874c03fab829d-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a3555115241_16.jpg",
       artist: "Delayde, anybodyy",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=23188",
       color: ["#B1F8A8", "#8A62D5"],
@@ -42,8 +41,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Viatecture",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2021/06/23b2ff959731b56ea8545828b462311e8b1a2bcc-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a2241183772_16.jpg",
       artist: "Leavv, Makzo",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=20119",
       color: ["#AEB884", "#894E50"],
@@ -52,8 +50,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Mario Kart",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2021/03/74d62bc9370a68e440c1b98eaf650344f0a7faea-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a0968437577_16.jpg",
       artist: "SwuM",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=15228",
       color: ["#283432", "#964A2A"],
@@ -62,8 +59,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Cabaña",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2021/07/4163ebb931e06d4ee8eb184295c0246d4a5055f7-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a4251697261_16.jpg",
       artist: "Juan Rios",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=21647",
       color: ["#FF8E73", "#3F2240"],
@@ -72,8 +68,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Cascade",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2021/02/70a56749b8b89815fa75446030c6ba57d2c34de7-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a2974450133_16.jpg",
       artist: "Knowmadic",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=13092",
       color: ["#015754", "#FB8B65"],
@@ -82,8 +77,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Dreamlover",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2021/02/d12927eedcc2f5afba2ab049a4a1ea46c2266ae3-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a0642934673_16.jpg",
       artist: "C Y G N",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=14995",
       color: ["#88BBEA", "#E58FF0"],
@@ -92,8 +86,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Under the City Stars",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a2013294891_16.jpg",
       artist: "Aso, Middle School, Aviino",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
       color: ["#174E4B", "#D67252"],
@@ -102,8 +95,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Foggy Road",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/5c3cb92a68f953925c92d259d8d7b52d3bd9aa06-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a3260161526_16.jpg",
       artist: "Toonorth",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=7834",
       color: ["#3BA46C", "#EF764B"],
@@ -112,8 +104,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Deeper",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/10/23fdd99adc3e16abcb67b004ea3e748ebf433a49-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a2720184939_16.jpg",
       artist: "Aviino",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=10460",
       color: ["#CE5FAC", "#5B9CDE"],
@@ -122,8 +113,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Seascape",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a2682262161_16.jpg",
       artist: "Makzo",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=11773",
       color: ["#4F64A3", "#D4877F"],
@@ -133,7 +123,7 @@ function chillHop() {
       id: uuidv4(),
       name: "After Hours",
       cover:
-        "https://chillhop.com/wp-content/uploads/2022/10/36fcc61f2d1c707e99f36706700d73229e2e8dff-1024x1024.jpg",
+        "https://cms.chillhop.com/media/35631/squarel36fcc61f2d1c707e99f36706700d73229e2e8dff.jpg",
       artist: "Poldoore",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=36942",
       color: ["#C79500", "#2B426C"],
@@ -142,8 +132,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Pampaya",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2022/05/d8344de22563af8eaec8f544c14711592eabef26-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a1734548951_16.jpg",
       artist: "Plusma, Guillaume Muschalle",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=28899",
       color: ["#69A0A3", "#FABC81"],
@@ -152,8 +141,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Higher",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2021/05/2473c60e471fe9b40e246bf7711c87d3d6ea69a7-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a3142568702_16.jpg",
       artist: "Misha, Jussi Halme",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=15088",
       color: ["#E87675", "#2E365B"],
@@ -162,8 +150,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Passing Notes",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2021/07/5821e04fd52fa668a0b9890f213cbb12e454cc6b-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a2966482886_16.jpg",
       artist: "Middle School, Aso",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=22938",
       color: ["#3D4F5B", "#CE6C49"],
@@ -172,8 +159,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "New Moon",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2021/05/732128e1da8fd5f6292ffc1926a2ea840a54f4d0-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a2760964854_16.jpg",
       artist: "Toonorth",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=17931",
       color: ["#FFE76D", "#9A9AFA"],
@@ -182,8 +168,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Lemon",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/53eef8b1ac9bf40038b2b5dc7ca2d13fc19cf95e-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a0833399127_16.jpg",
       artist: "Tom Doolie",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=7977",
       color: ["#7D2D36", "#655292"],
@@ -192,8 +177,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Vinho Verde",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ae91385fc2d92e40dd4bf337867dee16fb5648d2-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a3946106493_16.jpg",
       artist: "Clap Cotton",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=9447",
       color: ["#394F2B", "#E8F9B5"],
@@ -202,8 +186,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Waybackwhen",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2021/11/4c9682ee612a3b8ef51de286c49b5489408e9257-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a3230987507_16.jpg",
       artist: "Philanthrope, Parkbench Epiphany",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=27502",
       color: ["#5D7985", "#EF9796"],
@@ -213,7 +196,7 @@ function chillHop() {
       id: uuidv4(),
       name: "Changing Winds",
       cover:
-        "https://chillhop.com/wp-content/uploads/2022/06/312458b974ab45c795d75ec3f0259caa3989103a-1024x1024.jpg",
+        "https://cms.chillhop.com/media/41661/squarel312458b974ab45c795d75ec3f0259caa3989103a.jpg",
       artist: "Knowmadic",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=41654",
       color: ["#E7CE9C", "#DA888E"],
@@ -222,8 +205,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Ultraviolet",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2021/08/af3ce13ad39d38057f00144f8a7c295877ccfec1-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a0426240839_16.jpg",
       artist: "Smile High, Teddy Roxpin",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=23345",
       color: ["#A9DBD8", "#286577"],
@@ -232,8 +214,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Nectarine",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2022/10/2b9d337d372db542748e2643dc90af62dd3418e2-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a0040393927_16.jpg",
       artist: "Es-K, Danny James, John Culbreth",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=34242",
       color: ["#B10400", "#DFD0B3"],
@@ -242,8 +223,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Where I Met You",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/e2fe86f9be714c7e1e44f789d6d338c4a00c4668-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a3802435790_16.jpg",
       artist: "Moods",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=7975",
       color: ["#CE537F", "#E9D14B"],
@@ -252,8 +232,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Awakening",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/3599aa307a704376e6a680b13bdbdb37d76a83ed-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a1940514489_16.jpg",
       artist: "j'san",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=7953",
       color: ["#EC5894", "#55BAB6"],
@@ -262,8 +241,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Coda",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2021/02/7f102bdde417f6ead9a120b2b931449e5c12b4da-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a0620301508_16.jpg",
       artist: "Aarigod",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=13001",
       color: ["#9BCA3E", "#425037"],
@@ -272,8 +250,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Toft",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/858b533ba20ff95bf5b401089b195d1a8cb43870-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a4045406787_16.jpg",
       artist: "Felty, Ezzy",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=9312",
       color: ["#1971B1", "#F0DA39"],
@@ -282,8 +259,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Morning In The Cafe",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/9ba130514bcb5697e8a9be6a93fa2c8fc88223d0-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a0348155835_16.jpg",
       artist: "No Spirit",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=8068",
       color: ["#8C5A63", "#B3674F"],
@@ -292,8 +268,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Under Leaves",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/0b2270e2865d7c9fb7ce840263bc6fb93f64f812-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a1572188684_16.jpg",
       artist: "Mr. Käfer",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=9070",
       color: ["#289E8E", "#DD7C8F"],
@@ -302,8 +277,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Journeys",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/08/63d2d2cdabbc5df023603b5f47b2fb97963cb537-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a2771390192_16.jpg",
       artist: "Sitting Duck, squeeda",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=8650",
       color: ["#A79650", "#7E573A"],
@@ -312,8 +286,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Sleepover",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/09fb436604242df99f84b9f359acb046e40d2e9e-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a3959539658_16.jpg",
       artist: "Nymano, JK the Sage",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=10130",
       color: ["#F4A49D", "#6A5F8A"],
@@ -322,8 +295,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Paraglider",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/4fac6bb3a32f9e76b465990ba8d97d7db8a372f5-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a3888353364_16.jpg",
       artist: "Kendall Miles, H E R B",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=9318",
       color: ["#F19447", "#9F6B77"],
@@ -332,8 +304,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Passage",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/c3811ccd3dd069ebcecf80403921efd1bb690f49-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a0613837456_16.jpg",
       artist: "L'Indécis",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=8446",
       color: ["#61D0D7", "#DD0187"],
@@ -342,8 +313,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "la zona",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2022/10/3ff29a35be582c8dc0222273cb9c401ea6b209dc-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a1182733144_16.jpg",
       artist: "Maydee",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=27455",
       color: ["#782B21", "#A37716"],
@@ -352,8 +322,7 @@ function chillHop() {
     {
       id: uuidv4(),
       name: "Aqui",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/8fe97239f67b9eeb9ff0cc8833a5fdc0d99b3e9f-1024x1024.jpg",
+      cover: "https://f4.bcbits.com/img/a3913594238_16.jpg",
       artist: "Cloudchord, Soul Food Horns",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=8165",
       color: ["#9D5060", "#2C2E69"],
